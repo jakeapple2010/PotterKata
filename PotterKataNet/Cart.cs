1d043cf07b0fdc52250a2355f2e3cef0055dbca0
jakeapple2010 <jakeapple2010@gmail.com>
2017-06-05 12:22:58 -0600

    Initial solution complete. Needs much refactoring.

---
+++ PotterKataNet/Cart.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PotterKataNet
+{
+    public class Cart
+    {
+        public List<string> Books { get; private set; }
+
+        public void AddBooksToCard(List<string> booksToAddToCart)
+        {
+            Books = booksToAddToCart;
+        }
+
+        readonly Dictionary<int, decimal> _discounts = new Dictionary<int, decimal>
+        {
+            { 0, 0m },
+            { 1, 0m },
+            { 2, 0.05m },
+            { 3, 0.1m },
+            { 4, 0.20m },
+            { 5, 0.25m }
+        };
+
+        public decimal CalculateCost()
+        {
+            var totalCost = 0m;
+
+            while (Books.Any())
+            {
+                var bookSet = Books.Distinct().ToList();
+
+                if (HasTwoSetsOfFour())
+                {
+                    RemoveSetOfFour();
+                    RemoveSetOfFour();
+                    totalCost += 51.2m;
+                    continue;
+                }
+
+                RemoveBookSet(bookSet);
+                
+                if (_discounts.ContainsKey(bookSet.Count()))
+                {
+                    totalCost += bookSet.Count() * 8 * (1-_discounts[bookSet.Count()]);
+                }
+            }
+            return totalCost;
+        }
+
+        private bool HasTwoSetsOfFour()
+        {
+            var workingBookList = Books.ToList();
+            var enumerable = workingBookList.Distinct().Take(4).ToList();
+            foreach (var item in enumerable)
+            {
+                workingBookList.Remove(item);
+            }
+            return workingBookList.Distinct().Count() >= 4;
+        }
+
+        private void RemoveSetOfFour()
+        {
+            var enumerable = Books.Distinct().Take(4).ToList();
+            foreach (var item in enumerable)
+            {
+                Books.Remove(item);
+            }
+        }
+
+        private void RemoveBookSet(IEnumerable<string> bookSet)
+        {
+            foreach (var s in bookSet)
+            {
+                Books.Remove(s);
+            }
+        }
+    }
+}
---
+++ PotterKataNet/CartSpecs.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace PotterKataNet
+{
+    [TestFixture]
+    public class CartSpecs
+    {
+        private readonly Cart _classUnderTest = new Cart();
+
+        [Test]
+        public void Calculate_cost_of_no_books()
+        {
+            var books = new List<string> { };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(0m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_one_book()
+        {
+            var books = new List<string> { "book1" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(8m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_two_books()
+        {
+            var books = new List<string> { "book1", "book2" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(16m * .95m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_three_books()
+        {
+            var books = new List<string> { "book1", "book2", "book3" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(24m * .9m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_four_books()
+        {
+            var books = new List<string> { "book1", "book2", "book3", "book4" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(32m * .8m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_five_books()
+        {
+            var books = new List<string> { "book1", "book2", "book3", "book4", "book5" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(40m * .75m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_two_of_the_same_books()
+        {
+            var books = new List<string> { "book1", "book1" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(16m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_two_of_the_same_books_plus_another()
+        {
+            var books = new List<string> { "book1", "book2", "book1" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(16m * 0.95m + 8m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_two_of_book_one_plus_two_of_book_two()
+        {
+            var books = new List<string> { "book1", "book2", "book1", "book2" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(16m * 0.95m + 16m * 0.95m));
+        }
+
+        [Test]
+        public void Calculate_cost_of_two_of_first_second_and_third_and_one_of_fourth_and_fifth()
+        {
+            var books = new List<string> { "book1", "book1", "book2", "book2", "book3", "book3", "book4", "book5" };
+
+            _classUnderTest.AddBooksToCard(books);
+            var result = _classUnderTest.CalculateCost();
+
+            Assert.That(result, Is.EqualTo(51.2m));
+        }
+    }
+}
