1874ea19214057a7ffdcdb16d375b7ffd5c98465
jakeapple2010 <jakeapple2010@gmail.com>
2017-09-19 22:09:39 -0600

    Fix bug for two sets of five
    Optimal discount is 2 sets of 4 and 1 set of 2.

--- PotterKataNet/BookCartExtensions.cs
+++ PotterKataNet/BookCartExtensions.cs
@@ -5,9 +5,9 @@ namespace PotterKataNet
 {
     static class BookCartExtensions
     {
-        public static void RemoveSetOfFour(this ICollection<string> books)
+        public static void RemoveNumberOfBooks(this ICollection<string> books, int count)
         {
-            var enumerable = books.Distinct().Take(4).ToList();
+            var enumerable = books.Distinct().Take(count).ToList();
             foreach (var item in enumerable)
             {
                 books.Remove(item);
@@ -16,8 +16,14 @@ namespace PotterKataNet
 
         public static void RemoveTwoSetsOfFour(this ICollection<string> books)
         {
-            books.RemoveSetOfFour();
-            books.RemoveSetOfFour();
+            books.RemoveNumberOfBooks(4);
+            books.RemoveNumberOfBooks(4);
+        }
+
+        public static void RemoveTwoSetsOfFive(this ICollection<string> books)
+        {
+            books.RemoveNumberOfBooks(5);
+            books.RemoveNumberOfBooks(5);
         }
 
         public static bool HasTwoSetsOfFour(this ICollection<string> books)
@@ -29,6 +35,17 @@ namespace PotterKataNet
                 workingBookList.Remove(item);
             }
             return workingBookList.Distinct().Count() >= 4;
+        }
+
+        public static bool HasTwoSetsOfFive(this ICollection<string> books)
+        {
+            var workingBookList = books.ToList();
+            var enumerable = workingBookList.Distinct().Take(5).ToList();
+            foreach (var item in enumerable)
+            {
+                workingBookList.Remove(item);
+            }
+            return workingBookList.Distinct().Count() >= 5;
         }
 
         public static void RemoveBookSet(this ICollection<string> books, IEnumerable<string> booksToRemove)
